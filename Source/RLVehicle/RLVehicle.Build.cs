1329ae8330dacaecdb80629d33487bb782699a21
scottmei <scottmei@tencent.com>
2024-06-24 23:23:50 +0800

    Add Learning Agents plugin

--- Source/RLVehicle/RLVehicle.Build.cs
+++ Source/RLVehicle/RLVehicle.Build.cs
@@ -6,6 +6,7 @@ public class RLVehicle : ModuleRules
 {
 	public RLVehicle(ReadOnlyTargetRules Target) : base(Target)
 	{
+		PrivateDependencyModuleNames.AddRange(new string[] { "LearningAgents" });
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ChaosVehicles", "PhysicsCore" });
